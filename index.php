35c88ff8f5d38fe6d985a1c2dc208b1b89fad4ae
pzky9614 <43930163+pzky9614@users.noreply.github.com>
2024-11-09 14:14:44 -0600

    index.php

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 // Get the PHP helper library from https://twilio.com/docs/libraries/php
 
 // this line loads the library 
-require_once '/path/to/vendor/autoload.php';
+require_once 'vendor/autoload.php';
 use Twilio\TwiML\VoiceResponse;
 
 $response = new VoiceResponse;
 $response->say("Hello world!");
 print $response;
-?>
